3cebe53be23a8fc29fe52c30730611f9168ef869
Thanos <zinbiel@gmail.com>
2012-02-29 21:40:37 +0100

    converted tabs to spaces

--- geo.rb
+++ geo.rb
@@ -10,58 +10,58 @@ require 'open-uri'
 
 
 def getFileGps(fileName)
-	if fileName == nil
-		return nil
-	end
-	@fileGps = EXIFR::JPEG.new(fileName).gps
-	if (@fileGps == nil)
-		puts 'no gps info found'
-		return nil
-	end
-	return [@fileGps.latitude, @fileGps.longitude]
+    if fileName == nil
+        return nil
+    end
+    @fileGps = EXIFR::JPEG.new(fileName).gps
+    if (@fileGps == nil)
+        puts 'no gps info found'
+        return nil
+    end
+    return [@fileGps.latitude, @fileGps.longitude]
 end
 
 def downloadImage(url)
-	@localImage = '/tmp/image.jpg'
-	File.open(@localImage, "wb") do |saved_file|
-		begin
-			open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE) do |remoteFile|
-				if (remoteFile.content_type != 'image/jpeg')
-					puts 'invalid content type:' + remoteFile.content_type
-					return nil
-				end
-				saved_file.write(remoteFile.read)
-			return @localImage
-			end
-	  	rescue
-	  		return nil
-		end
-	end
-	return nil
+    @localImage = '/tmp/image.jpg'
+    File.open(@localImage, "wb") do |saved_file|
+        begin
+            open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE) do |remoteFile|
+                if (remoteFile.content_type != 'image/jpeg')
+                    puts 'invalid content type:' + remoteFile.content_type
+                    return nil
+                end
+                saved_file.write(remoteFile.read)
+            return @localImage
+            end
+        rescue
+            return nil
+        end
+    end
+    return nil
 end
 
 
 include Geokit::Geocoders
 
 if (0 === ARGV.count) 
-	puts "try some files as parameters"
+    puts "try some files as parameters"
 end 
 
 
 ARGV.each do |imageFile|
-	if (imageFile.match('http'))
-		imageFile = downloadImage(imageFile)
-	end
+    if (imageFile.match('http'))
+        imageFile = downloadImage(imageFile)
+    end
 
-	if (imageFile === nil || !File::exists?(imageFile))
-		puts 'Invalid file : ' + imageFile
-		exit
-	end
+    if (imageFile === nil || !File::exists?(imageFile))
+        puts 'Invalid file : ' + imageFile
+        exit
+    end
 
-	@gps = getFileGps(imageFile)
-	
-	if @gps != nil
-		puts GoogleGeocoder.reverse_geocode(@gps).full_address
-	end
+    @gps = getFileGps(imageFile)
+    
+    if @gps != nil
+        puts GoogleGeocoder.reverse_geocode(@gps).full_address
+    end
 end
 
